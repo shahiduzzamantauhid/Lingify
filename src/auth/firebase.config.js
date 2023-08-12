430ef2cf60fe9ce7f5713e635ba6b58037281bbd
Md. Rifat Hossen Saown <rifatsaown0@gmail.com>
2023-08-13 01:44:26 +0600

    Firebase Config Added\

--- src/auth/firebase.config.js
+++ src/auth/firebase.config.js
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.Lingify_apiKey, 
-  authDomain: import.meta.env.Lingify_authDomain, 
-  projectId: import.meta.env.Lingify_projectId, 
-  storageBucket: import.meta.env.Lingify_storageBucket, 
-  messagingSenderId: import.meta.env.Lingify_messagingSenderId, 
-  appId: import.meta.env.Lingify_appId
+  apiKey: import.meta.env.VITE_LINGIFY_apiKey, 
+  authDomain: import.meta.env.VITE_LINGIFY_authDomain, 
+  projectId: import.meta.env.VITE_LINGIFY_projectId, 
+  storageBucket: import.meta.env.VITE_LINGIFY_storageBucket, 
+  messagingSenderId: import.meta.env.VITE_LINGIFY_messagingSenderId, 
+  appId: import.meta.env.VITE_LINGIFY_appId
 };
 
 // Initialize Firebase
